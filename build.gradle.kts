373cb5cf1ce50ec809d6f21661f1a4da1a85d50c
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-05-31 12:32:17 +0200

    Add ktlint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // TODO: remove buildscript{} block if it contains nothing but comments
 buildscript {
     // Don't declare classpath dependencies here.
@@ -6,7 +9,21 @@ buildscript {
     // we can use type-safe kotlin scripts defined in `buildSrc` sources.
 }
 
-// TODO: add ktlint plugin
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0" // Run: .\gradlew ktlintFormat
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    configure<KtlintExtension> {
+        debug.set(true)
+        ignoreFailures.set(true)
+        disabledRules.set(setOf("no-wildcard-imports"))
+        reporters {
+            reporter(ReporterType.CHECKSTYLE)
+        }
+    }
+}
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
