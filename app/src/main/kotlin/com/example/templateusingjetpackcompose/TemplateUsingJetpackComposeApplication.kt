44a19c1464596942063d467d483dbbc6680c15d9
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-06-10 12:01:39 +0200

    Prepare application and activity classes to use Hilt

---
+++ app/src/main/kotlin/com/example/templateusingjetpackcompose/TemplateUsingJetpackComposeApplication.kt
@@ -0,0 +1,7 @@
+package com.example.templateusingjetpackcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TemplateUsingJetpackComposeApplication : Application()
