625f9b0779f59b3f557f9cfd42a28a4a358b3856
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-06-30 10:35:18 +0200

    Remove 'object Loading' from Result as it doesn't represent a result

--- core-data/src/main/kotlin/com/example/core/data/repository/CatsRepositoryImpl.kt
+++ core-data/src/main/kotlin/com/example/core/data/repository/CatsRepositoryImpl.kt
@@ -5,7 +5,10 @@ import com.example.core.model.Cat
 import com.example.core.network.model.NetworkCat
 import com.example.core.network.restApi.NetworkCatsDataSource
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class CatsRepositoryImpl @Inject constructor(
@@ -16,7 +19,6 @@ class CatsRepositoryImpl @Inject constructor(
         return flow<Result<List<Cat>>> {
             emit(Result.Success(networkCatsDataSource.getCats().asCat()))
         }
-            .onStart { emit(Result.Loading) }
             .catch { emit(Result.Error(it)) }
             .flowOn(Dispatchers.IO)
     }
