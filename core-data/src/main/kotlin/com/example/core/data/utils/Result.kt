e9b760da1fc5168e10d6134b5c3a8a799d724d3d
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-06-30 11:43:26 +0200

    Add ErrorHandler errorMessage

--- core-data/src/main/kotlin/com/example/core/data/utils/Result.kt
+++ core-data/src/main/kotlin/com/example/core/data/utils/Result.kt
@@ -2,5 +2,5 @@ package com.example.core.data.utils
 
 sealed interface Result<out T> {
     data class Success<T>(val data: T) : Result<T>
-    data class Error(val exception: Throwable? = null) : Result<Nothing>
+    data class Error(val errorMessage: String) : Result<Nothing>
 }
