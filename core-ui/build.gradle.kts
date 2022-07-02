bcab2095846ec4558c7660d319abf79abb34f855
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-07-02 15:16:39 +0200

    Add common loading screen

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -10,6 +10,8 @@ dependencies {
     implementation(Libs.AndroidX.Compose.foundation)
     implementation(Libs.AndroidX.Compose.ui)
     implementation(Libs.AndroidX.Compose.material)
+    debugImplementation(Libs.AndroidX.Compose.tooling)
+    implementation(Libs.AndroidX.Compose.toolingPreview)
     implementation(Libs.AndroidX.Lifecycle.runtimeKtx)
 
     testImplementation(Libs.JUnit.junit)
