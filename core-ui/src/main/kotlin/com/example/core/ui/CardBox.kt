df3e2fabe1f64b703cd5d86b1d4cd1586fa3d70e
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-07-02 16:15:54 +0200

    Add common card box

---
+++ core-ui/src/main/kotlin/com/example/core/ui/CardBox.kt
@@ -0,0 +1,46 @@
+package com.example.core.ui
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardBox(
+    modifier: Modifier = Modifier,
+    @DrawableRes iconId: Int,
+    text: String,
+) {
+    Card(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = iconId),
+                contentDescription = "Cat icon",
+                modifier = Modifier.align(Alignment.CenterStart),
+                tint = Color.Unspecified
+            )
+            Text(
+                text = text,
+                style = MaterialTheme.typography.h4,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+}
