56122c27cec30206a942203dc163d6e980706b77
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-07-07 13:45:03 +0200

    Implement top app bar

--- feature-cats/src/main/kotlin/com/example/feature/cats/CatsScreen.kt
+++ feature-cats/src/main/kotlin/com/example/feature/cats/CatsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -18,10 +19,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.core.model.Cat
-import com.example.core.ui.CardBox
-import com.example.core.ui.LoadingScreen
+import com.example.core.ui.*
 import com.example.core.ui.R
-import com.example.core.ui.rememberFlowWithLifecycle
 
 @Composable
 fun CatsRoute(
@@ -67,20 +66,24 @@ fun CatsContent(
     cats: List<Cat>,
     onCatClick: (String) -> Unit,
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)
-
+    Scaffold(
+        topBar = { CatsTopAppBar(title = "Cats") }
     ) {
-        items(cats) {
-            CardBox(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .clickable { onCatClick(it.id) },
-                iconId = R.drawable.ic_cat,
-                text = it.tags.first()
-            )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
+
+        ) {
+            items(cats) {
+                CardBox(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { onCatClick(it.id) },
+                    iconId = R.drawable.ic_cat,
+                    text = it.tags.first()
+                )
+            }
         }
     }
 }
