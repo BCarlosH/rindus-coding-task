b1308301ff1d1442354437a817b52fa6a13d1565
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-07-07 13:44:44 +0200

    Create top app bar

---
+++ core-ui/src/main/kotlin/com/example/core/ui/TopAppBar.kt
@@ -0,0 +1,38 @@
+package com.example.core.ui
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CatsTopAppBar(
+    title: String,
+    elevation: Dp = AppBarDefaults.TopAppBarElevation,
+    icon: @Composable (() -> Unit)? = null,
+    onBackClick: () -> Unit = {},
+) {
+    val navigationIcon: (@Composable () -> Unit)? = if (icon == null) {
+        null
+    } else {
+        { IconButton(onClick = onBackClick) { icon() } }
+    }
+
+    TopAppBar(
+        title = { Text(title, fontSize = 18.sp) },
+        navigationIcon = navigationIcon,
+        elevation = elevation
+    )
+}
+
+@Preview
+@Composable
+private fun SimpleTopBarPreview() {
+    CatsTopAppBar(
+        title = "CatsTopAppBar",
+        icon = { Icon(Icons.Filled.ArrowBack, "Back") }
+    )
+}
