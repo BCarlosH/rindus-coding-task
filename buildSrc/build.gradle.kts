44f6dc39b210b3541ce432e859c758a4d089d51c
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-08-06 12:42:41 +0200

    Update Dagger Hilt plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,5 +15,5 @@ kotlinDslPluginOptions {
 dependencies {
     implementation("com.android.tools.build:gradle:7.2.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
-    implementation("com.google.dagger:hilt-android-gradle-plugin:2.42")
+    implementation("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
 }
