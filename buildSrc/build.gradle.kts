146c24897477d945784ab8bf998bc80f815048e8
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-08-06 10:49:13 +0200

    Bump up Gradle build tools

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.1")
+    implementation("com.android.tools.build:gradle:7.2.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.42")
 }
