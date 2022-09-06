310b39ae09fd396a017c384c92698214e66f7b5c
Carlos Hernandez <c.hernandez.fch@gmail.com>
2022-09-06 17:56:03 +0200

    Make the loading view clickable with no effect and add default background color

--- core-ui/src/main/kotlin/com/rinduscodingtask/core/ui/LoadingScreen.kt
+++ core-ui/src/main/kotlin/com/rinduscodingtask/core/ui/LoadingScreen.kt
@@ -1,9 +1,14 @@
 package com.rinduscodingtask.core.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,6 +19,10 @@ fun LoadingScreen(
 ) {
     Box(
         modifier = modifier
+            .background(MaterialTheme.colors.background)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }) { }
     ) {
         Column(
             modifier = Modifier.align(Alignment.Center),
